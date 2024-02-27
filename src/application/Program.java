50e2cd58cb629af72375d02049748d19b1b974fd
Argeu Limbo <argeusousaptbr@gmail.com>
2024-02-27 16:26:20 -0300

    Project AccountBank: primeiros arquivos

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Account account = new Account();
+		
+		
+		System.out.println("Enter account data: ");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.next();
+		String holder = sc.nextLine();		
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw Limit: ");
+		double withdrawLimit = sc.nextDouble();
+		System.out.println();
+		account = new Account(number, holder, balance, withdrawLimit);
+		System.out.println(account.toString());
+		
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			account.withdraw(amount);
+		} catch (DomainException insuficientBalance) {
+			System.out.println("Insuficient balance for make the withdraw!");
+		}
+		System.out.println(account.toString());
+		
+		sc.close();
+	}
+}
---
+++ src/model/entities/Account.java
@@ -0,0 +1,74 @@
+package model.entities;
+
+import model.exceptions.DomainException;
+
+public class Account {
+	
+	protected Integer number;
+	protected String holder;
+	protected Double balance;
+	protected Double withdrawLimit;
+	
+	public Account() {
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(Double amount) {
+		this.balance += amount;
+	}
+	
+	public void withdraw(Double amount) throws DomainException {
+		if (this.balance < this.withdrawLimit) {
+			throw new DomainException("Insuficient balance for make the withdraw!");
+		}
+		this.balance -= amount;
+		this.withdrawLimit -= amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account data - Number: " + getNumber()
+		       + ", Holder: " + getHolder()
+		       + ", Balance: R$ " + getBalance()
+		       + ", Withdraw Limit: R$ " + getWithdrawLimit();
+	}
+	
+}
